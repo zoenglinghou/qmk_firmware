eafccf9c5355fe3a614c726d6cf03d61cf13f052
Linghao Zhang <linghao.zhang@protonmail.com>
2023-08-14 16:50:59 +0200

    Remove combo, turn capslock to fn

--- keyboards/keychron/v1/ansi/keymaps/zoenglinghou/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/zoenglinghou/keymap.c
@@ -21,8 +21,8 @@
 enum layers{
     MAC_BASE,
     MAC_FN,
-    LINUX_BASE,
-    LINUX_FN
+    LNX_BASE,
+    LNX_FN
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -45,38 +45,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 
-    [LINUX_BASE] = LAYOUT_ansi_82(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_END,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(LINUX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+    [LNX_BASE] = LAYOUT_ansi_82(
+        KC_ESC,     KC_F1,      KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_END,
+        KC_GRV,     KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,     KC_Q,       KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        MO(LNX_FN), KC_A,       KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        KC_LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        KC_LCTL,    KC_LWIN,    KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(LNX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-    [LINUX_FN] = LAYOUT_ansi_82(
+    [LNX_FN] = LAYOUT_ansi_82(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            KC_PSCR,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                DB_TOGG,                                _______,  _______,    _______,  _______,  _______,  _______),
-};
-
-// Custom combo
-
-enum combos {
-    ALT_C_COPY,
-    ALT_C_PASTE,
-    COMBO_LENGTH,
-};
-
-uint16_t COMBO_LEN = COMBO_LENGTH;
-
-// Mac-Like copy-an-paste
-const uint16_t PROGMEM mac_copy[] = {KC_LALT, KC_C, COMBO_END};
-const uint16_t PROGMEM mac_paste[] = {KC_LALT, KC_V, COMBO_END};
-
-combo_t key_combos[] = {
-    [ALT_C_COPY] = COMBO(mac_copy, KC_COPY),
-    [ALT_C_PASTE] = COMBO(mac_paste, KC_PASTE),
 };
