023597c9e26c9d1bab45a428af7a341d6533459e
Linghao Zhang <linghao.zhang@protonmail.com>
2023-08-14 16:50:59 +0200

    Remove combo, turn capslock to fn

--- keyboards/keychron/q5/ansi_encoder/keymaps/zoenglinghou/keymap.c
+++ keyboards/keychron/q5/ansi_encoder/keymaps/zoenglinghou/keymap.c
@@ -22,8 +22,8 @@
 enum layers{
   MAC_BASE,
   MAC_FN,
-  LINUX_BASE,
-  LINUX_FN
+  LNX_BASE,
+  LNX_FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,  _______,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______),
-    [LINUX_BASE] = LAYOUT_ansi_98(
-        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   KC_HOME,  KC_END,   KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_P7,    KC_P8,    KC_P9,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(LINUX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
-    [LINUX_FN] = LAYOUT_ansi_98(
+    [LNX_BASE] = LAYOUT_ansi_98(
+        KC_ESC,                 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   KC_HOME,  KC_END,   KC_MUTE,
+        KC_GRV,     KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,     KC_Q,       KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_P7,    KC_P8,    KC_P9,
+        MO(LNX_FN), KC_A,       KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,                KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
+        KC_LCTL,    KC_LWIN,    KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(LNX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
+    [LNX_FN] = LAYOUT_ansi_98(
         _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,    KC_VOLD,  KC_VOLU,            _______,  KC_PSCR,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [LINUX_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [LINUX_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [LNX_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LNX_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
 
@@ -75,22 +75,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
-
-// Custom combo
-
-enum combos {
-    ALT_C_COPY,
-    ALT_C_PASTE,
-    COMBO_LENGTH,
-};
-
-uint16_t COMBO_LEN = COMBO_LENGTH;
-
-// Mac-Like copy-an-paste
-const uint16_t PROGMEM mac_copy[] = {KC_LALT, KC_C, COMBO_END};
-const uint16_t PROGMEM mac_paste[] = {KC_LALT, KC_V, COMBO_END};
-
-combo_t key_combos[] = {
-    [ALT_C_COPY] = COMBO(mac_copy, KC_COPY),
-    [ALT_C_PASTE] = COMBO(mac_paste, KC_PASTE),
-};
--- keyboards/keychron/q5/ansi_encoder/keymaps/zoenglinghou/rules.mk
+++ keyboards/keychron/q5/ansi_encoder/keymaps/zoenglinghou/rules.mk
@@ -4,5 +4,4 @@ ENCODER_MAP_ENABLE = yes
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
 
-COMBO_ENABLE = yes
 CONSOLE_ENABLE = yes
